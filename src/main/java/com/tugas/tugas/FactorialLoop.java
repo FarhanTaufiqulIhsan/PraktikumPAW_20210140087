6a86ba02246d783462e6620d703a871a106a9e0c
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2023-08-05 10:03:22 +0700

    FactorialLoop

---
+++ src/main/java/com/tugas/tugas/FactorialLoop.java
@@ -0,0 +1,13 @@
+package com.tugas.tugas;
+
+public class FactorialLoop {
+
+    static int factorial(int value) {
+        var result = 1;
+        for (int i = 1; i <= value; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+}
